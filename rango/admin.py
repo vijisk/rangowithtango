3d302553365ed0a19edd1e67e18186d5b75a072d
vlakshmi22 <vlakshmi22@yahoo.co.in>
2021-12-15 16:45:31 +0000

    Done with Models and Database

--- rango/admin.py
+++ rango/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 from rango.models import Category, Page
 
+class PageAdmin(admin.ModelAdmin):
+    list_display = ("title", "url", "category")
+    
+
 admin.site.register(Category)
-admin.site.register(Page)
+admin.site.register(Page, PageAdmin)
 
 
 
