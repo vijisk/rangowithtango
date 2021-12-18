150d0d7b38ad94305abd64b6c6ada3458e5662d7
vlakshmi22 <vlakshmi22@yahoo.co.in>
2021-12-18 09:02:51 +0000

    using url template

--- rango/forms.py
+++ rango/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from rango.models import Category, Page
 
 class CategoryForm(forms.ModelForm):
-    name = forms.CharField(max_length=128, help_text="Please enter the category name.")
+    name = forms.CharField(max_length=Category.NAME_MAX_LENGTH, help_text="Please enter the category name.")
     likes = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
     views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
     slug = forms.CharField(widget=forms.HiddenInput(), required=False)
@@ -13,8 +13,8 @@ class CategoryForm(forms.ModelForm):
 
 
 class PageForm(forms.ModelForm):
-    title = forms.CharField(max_length=128, help_text="Please enter the title of page.")
-    url = forms.URLField(max_length=200, help_text="Please enter URL of the page.")
+    title = forms.CharField(max_length=Page.TITLE_MAX_LENGTH, help_text="Please enter the title of page.")
+    url = forms.URLField(max_length=Page.URL_MAX_LENGTH, help_text="Please enter URL of the page.")
     views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
 
     class Meta:
